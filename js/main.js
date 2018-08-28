fc746915a4a0ea870ca750ef8d6fa04bd3816a9d
Rafaela Georgescu <rafaela.georgescu@accesa.eu>
2018-08-28 14:34:52 +0300

    my first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showStock(){
+    document.getElementById('stockForm').style.display ='block';
+    document.getElementById('personalForm').style.display = 'none';
+}
+function showPersonal(){
+    document.getElementById('personalForm').style.display = 'block';
+    document.getElementById('stockForm').style.display ='none';
+}
