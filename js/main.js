31ac73907e38ee3a36a5701887cb4e81bc23c940
rafaela.georgescu <rafigeorgescu@gmail.com>
2019-02-08 01:26:36 +0200

    ceva

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+// Get a reference to the database service
+var database = firebase.database();
+
+
+mounted() {
+
+    var query = database.ref('produse/'+ numeProdus +'/'); // shortened
+
+    query.once('value').then((snapshot) => {
+
+            this.productName = snapshot.child('productName').val();
+    });
+};
+
 function showStock(){
     document.getElementById('stockForm').style.display ='block';
     document.getElementById('personalForm').style.display = 'none';
 }
+
 function showPersonal(){
     document.getElementById('personalForm').style.display = 'block';
     document.getElementById('stockForm').style.display ='none';
